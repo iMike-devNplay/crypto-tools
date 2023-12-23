5072370f86bc8c94866b6eb939fabb153cf57198
michael <imike.devnplay@gmail.com>
2023-12-23 23:02:17 +0100

    change cors config

---
+++ backend/src/main/java/fr/home/mikedev/cryptotools/dtos/security/basic/BasicAuthSecurityConfig.java
@@ -0,0 +1,25 @@
+package fr.home.mikedev.cryptotools.dtos.security.basic;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Order(1)
+public class BasicAuthSecurityConfig 
+{
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://www.mikedev.fr", "https://mikedev.fr"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: "https://www.mikedev.fr/crypto"
+  apiURL: "https://www.mikedev.fr/crypto-tools"
 };
