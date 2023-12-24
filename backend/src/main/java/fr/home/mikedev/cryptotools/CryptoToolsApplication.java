d41d069b6f8e5917f4ee72cb2a9cb579a0e7cc21
michael <imike.devnplay@gmail.com>
2023-12-24 10:56:58 +0100

    add ajp config

--- backend/src/main/java/fr/home/mikedev/cryptotools/CryptoToolsApplication.java
+++ backend/src/main/java/fr/home/mikedev/cryptotools/CryptoToolsApplication.java
@@ -1,13 +1,35 @@
 package fr.home.mikedev.cryptotools;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CryptoToolsApplication {
 
+    @Value("${tomcat.ajp.port}") int ajpPort;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoToolsApplication.class, args);
 	}
-
+	
+    @Bean
+    public ServletWebServerFactory servletContainer() 
+    {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(ajpPort);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        ((AbstractAjpProtocol<?>) ajpConnector.getProtocolHandler()).setSecret("Thisasecretbetweenapacheandtomcat");
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+ 
+        return tomcat;
+    }
 }
